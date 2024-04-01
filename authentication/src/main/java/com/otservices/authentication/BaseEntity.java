e05dad47b06f66c433815f7d72b4e58900636a46
groankur <36683555+groankur@users.noreply.github.com>
2024-04-01 14:54:56 +0000

    commit

---
+++ authentication/src/main/java/com/otservices/authentication/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.otservices.authentication;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+    
+
+
+
+
+}
